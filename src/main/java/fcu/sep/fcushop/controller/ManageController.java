05ec09cea76f5d68d13b191a970bfa73c598c8ba
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2021-12-05 18:26:11 +0800

    Fix check style

--- src/main/java/fcu/sep/fcushop/controller/ManageController.java
+++ src/main/java/fcu/sep/fcushop/controller/ManageController.java
@@ -4,48 +4,64 @@ import fcu.sep.fcushop.database.Sql2oDbHandler;
 import fcu.sep.fcushop.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.sql2o.Connection;
 
-import java.util.List;
 
+/**
+ * This is class ManageController.
+ */
 @RestController
 public class ManageController {
 
-    @Autowired
-    private Sql2oDbHandler sql2oDbHandler;
+  @Autowired
+  private Sql2oDbHandler sql2oDbHandler;
 
 
-    @Controller
-    public class OneController {
-        @RequestMapping("/manageFile")
-        public String manageFile() {
-            return "redirect:/manageFile.html";
-        }
+  /**
+   *  This is class OneController.
+   */
+  @Controller
+  public class OneController {
+    @RequestMapping("/manageFile")
+    public String manageFile() {
+      return "redirect:/manageFile.html";
     }
+  }
 
-    @Controller
-    public class TwoController {
-        @PostMapping("/addFile")
-        public String getNewData(@ModelAttribute("product") Product data) {
-            System.out.println(data.getId());
-            System.out.println(data.getName());
-            System.out.println(data.getImageUrl());
-            System.out.println(data.getDescription());
-            System.out.println(data.getPrice());
+  /**
+   *  This is class TwoController.
+   */
+  @Controller
+  public class TwoController {
 
-            try (Connection connection = sql2oDbHandler.getConnector().open()) {
-                String query = "insert into product (id,name,image_url,price,description)" +
-                                "values(NULL,:name,:image_url,:price,:description)";
-                connection.createQuery(query)
-                    .addParameter("name",data.getName())
-                    .addParameter("image_url", data.getImageUrl())
-                    .addParameter("price",  data.getPrice())
-                    .addParameter("description", data.getDescription())
-                    .executeUpdate();
+    /**
+     * This is getNewData function.
+     * @ param data form login website.
+     * @ return index.html;
+     */
+    @PostMapping("/addFile")
+     public String getNewData(@ModelAttribute("product") Product data) {
+      System.out.println(data.getId());
+      System.out.println(data.getName());
+      System.out.println(data.getImageUrl());
+      System.out.println(data.getDescription());
+      System.out.println(data.getPrice());
+      try (Connection connection = sql2oDbHandler.getConnector().open()) {
+        String query = "insert into product (id,name,image_url,price,description)"
+                        + "values(NULL,:name,:image_url,:price,:description)";
+        connection.createQuery(query)
+            .addParameter("name", data.getName())
+            .addParameter("image_url", data.getImageUrl())
+            .addParameter("price", data.getPrice())
+            .addParameter("description", data.getDescription())
+            .executeUpdate();
+      }
 
-            }
-            return "redirect:/index.html";
-        }
+      return "redirect:/index.html";
     }
+  }
 }
