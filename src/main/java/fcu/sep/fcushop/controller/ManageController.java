c0f4bc28c5963c7e72cf98f1a82c44622e77ef08
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2021-12-27 01:05:17 +0800

    test Delete

--- src/main/java/fcu/sep/fcushop/controller/ManageController.java
+++ src/main/java/fcu/sep/fcushop/controller/ManageController.java
@@ -102,8 +102,13 @@ public class ManageController {
     @PostMapping("/deleteFile")
     public String deleteData(@RequestBody Map params) {
       System.out.println("deleteDataController" + params.get("id").toString());
-      manageService.deleteData(params);
-
+      //manageService.deleteData(params);
+      try (Connection connection = sql2oDbHandler.getConnector().open()) {
+            String query = "DELETE FROM product WHERE id = :id";
+            return connection.createQuery(query)
+                    .addParameter("id", id)
+                    .executeUpdate();
+      }
       return "你好";
     }
   }
