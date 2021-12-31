a6874015bc9481d488cc1e443a3a8e66e9e424e3
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2021-12-31 19:34:53 +0800

    add Cart search Product function TEST

--- src/main/java/fcu/sep/fcushop/controller/ProductController.java
+++ src/main/java/fcu/sep/fcushop/controller/ProductController.java
@@ -1,13 +1,15 @@
 package fcu.sep.fcushop.controller;
 
+import fcu.sep.fcushop.database.Sql2oDbHandler;
+import fcu.sep.fcushop.model.Account;
 import fcu.sep.fcushop.model.Product;
 import fcu.sep.fcushop.service.ProductService;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.sql2o.Connection;
 
 
 /**
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
   @Autowired
+  private Sql2oDbHandler sql2oDbHandler;
   ProductService productManager;
 
   @GetMapping("/products")
@@ -31,4 +34,18 @@ public class ProductController {
     return productManager.getProducts(keyword);
   }
 
+  @PostMapping("/productsOfCart")
+  public String getProductsOfCart(@RequestBody Map params) {
+    Object all = params.get("cartItems").toString();
+    System.out.println(all);
+
+//    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+//      String query = "SELECT * FORM product WHERE ID = :id";
+//      connection.createQuery(query)
+//              .addParameter("username", data.getUsername())
+//              .addParameter("password", data.getPassword())
+//              .executeUpdate();
+//    }
+    return "hehe";
+  }
 }
