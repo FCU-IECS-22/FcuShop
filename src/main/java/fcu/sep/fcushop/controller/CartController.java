d4be695df53ee206192f9901c3563b9df116c22e
fcu-d0843556 <kevinfinity@mail.fcu.edu.tw>
2022-01-06 14:22:53 +0800

    add new function: cartItems

--- src/main/java/fcu/sep/fcushop/controller/CartController.java
+++ src/main/java/fcu/sep/fcushop/controller/CartController.java
@@ -1,6 +1,8 @@
 package fcu.sep.fcushop.controller;
 
 import fcu.sep.fcushop.database.Sql2oDbHandler;
+import fcu.sep.fcushop.model.Account;
+import fcu.sep.fcushop.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.sql2o.Connection;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -19,18 +24,16 @@ public class CartController {
     private Sql2oDbHandler sql2oDbHandler;
 
     @PostMapping("/productsOfCart")
-    public Object getProductsOfCart(@RequestBody Map params) {
-        Object all = params.get("cartItems");
-
-        System.out.println("cartItems : ");
-        System.out.println(all);
+    public List<Product> getProductsOfCart(@RequestBody Map params) {
+        ArrayList all = (ArrayList) params.get("cartItems");
 
         try (Connection connection = sql2oDbHandler.getConnector().open()) {
-          String query = "SELECT * FROM PRODUCT WHERE JSON_CONTAINS(id, ID)";
-          Object find = connection.createQuery(query)
-                  .executeUpdate();
+          String query = "SELECT ID id, NAME name, IMAGE_URL imageUrl, PRICE price,"
+                  + "DESCRIPTION description FROM PRODUCT WHERE ID IN(:arrayID)";
 
-          return find;
+          return connection.createQuery(query)
+                  .addParameter("arrayID",all)
+                  .executeAndFetch(Product.class);
         }
 
     }
